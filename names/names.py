5f0c71a77e2513dbfb345469e3c3cb61ac1d4659
AmarachiOE <ao.ejiawoko@gmail.com>
2019-07-26 09:37:17 -0700

    Using sets to return duplicate names. Faster time.

--- names/names.py
+++ names/names.py
@@ -10,7 +10,7 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
+# duplicates = []
 
 # ORIGINAL = 10 seconds
 # for name_1 in names_1:
@@ -19,10 +19,12 @@ duplicates = []
 #             duplicates.append(name_1)
 
 # AMARACHI1 = 2 seconds
-for name_1 in names_1:
-    if name_1 in names_2:
-        duplicates.append(name_1)
+# for name_1 in names_1:
+#     if name_1 in names_2:
+#         duplicates.append(name_1)
 
+# USING SET
+duplicates = set(names_1) & set(names_2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -31,4 +33,5 @@ print (f"runtime: {end_time - start_time} seconds")
 # Run Times 
 # Original = 10 seconds
 # Amarachi's = 2 seconds
+# Using Set = 0.006 seconds
 
